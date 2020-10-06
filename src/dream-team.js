dd4cd45dcfc9d8473bd25ad027ffd181f663e21e
Fialkaksu <fialkaksu1984@gmail.com>
2020-10-06 11:49:44 +0300

    feat: implement dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let dreamTeam = [];
+  if (members && members instanceof Array) {
+    members.forEach(member => {
+      if (member &&
+        typeof member === 'string') {
+        dreamTeam.push(member.trim().charAt(0).toUpperCase());
+      }
+    });
+
+    return dreamTeam.sort().join('');
+  } else return false;
 };
