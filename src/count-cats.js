5a9559f766d73f533b5c87dbe0dc089bfeeb92d7
Fialkaksu <fialkaksu1984@gmail.com>
2020-10-06 11:08:43 +0300

    feat: implement carbon-dating

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,13 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(backyard) {
-  // throw new CustomError('Not implemented');
-  // remove line with error and write your code here
   let count = 0;
 
   backyard.forEach(arr => {
     arr.forEach(el => {
-      if(el === '^^'){
+      if (el === '^^') {
         count += 1;
       }
     })
