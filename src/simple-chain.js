ff77f758623b8220c508f8b467cedc53cbb23c06
Fialkaksu <fialkaksu1984@gmail.com>
2020-10-07 11:16:52 +0300

    feat: implement simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chains: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chains.length;
   },
+
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    value === undefined ? this.chains.push(' ') : this.chains.push(`${value}`);
+    return this;
   },
+
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (position &&
+      typeof position === 'number' &&
+      parseInt(position) === position &&
+      position > 0 &&
+      position <= this.getLength()) {
+      this.chains.splice(position - 1, 1);
+    } else {
+      this.chains = [];
+      throw new Error();
+    }
+    return this;
   },
+
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chains.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    const chainsToPrint = `( ${this.chains.join(' )~~( ')} )`;
+    this.chains = [];
+    return chainsToPrint;
   }
 };
 
